1f029d2be9ed731e601a6775983734d54fea0e86
JOHNMITIAM <johnjohnmitiam14@gmail.com>
2023-07-27 21:32:09 +0800

    removed

--- src/modules/billing/pages/Layout/Layout.tsx
+++ src/modules/billing/pages/Layout/Layout.tsx
@@ -1,21 +1,15 @@
-import { useNavigate } from 'react-router-dom';
+// import { useNavigate } from 'react-router-dom';
 
-import { routes } from '@config/routes';
+// import { routes } from '@config/routes';
 
 interface IProps {
   children: React.ReactNode;
 }
 
 export const Layout: React.FC<IProps> = ({ children }) => {
-  const navigate = useNavigate();
   return (
     <>
-      <div
-        className="bg-sky-900 py-8"
-        onClick={() => {
-          navigate(routes.CONTACTS);
-        }}
-      >
+      <div className="bg-sky-900 py-8">
         <h1 className="text-4xl text-center text-yellow-100">Contact App</h1>
       </div>
       <div className="bg-slate-200 mx-auto">{children}</div>
